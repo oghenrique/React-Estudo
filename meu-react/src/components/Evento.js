7c84bd58aa2098d1838bfe58fbf9a72b916b32ff
Gustavo <santosguhenrique@gmail.com>
2024-08-22 11:51:10 -0300

    aula 11

--- meu-react/src/components/Evento.js
+++ meu-react/src/components/Evento.js
@@ -1,13 +1,19 @@
-function Evento({numero}){
+import Button from "./evento/Button"
+
+function Evento(){
 
     function meuEvento(){
-        console.log(`teste zanoniiiiiii ${numero}`)
+        console.log(`Ativando meu primeiro evento`)
     }
 
+    function segundoEvento(){
+        console.log(`Ativando meu segundo evento`)
+    }
     return (
         <>
         <p>Clique para disparar um evento:</p>
-        <button onClick={meuEvento}>Ativar!</button>
+        <Button event={meuEvento} text="Primeiro evento" />
+        <Button event={segundoEvento} text="Segundo evento" />
         </>
     )
 }
